cec5d153cdf9910df5a0e30dfb8e8970d991f557
TEXTBr <FireWitch2003@outlook.com>
2022-12-05 01:37:01 -0300

    testes com a API

--- js/home.js
+++ js/home.js
@@ -9,6 +9,14 @@
 // })
 
 // .catch(() => window.alert(`error`));
+let pai_de_todos = document.getElementById("home_jogos");
+let corpo = document.createElement("div");
+corpo.id = "jogo_1"
+corpo.className = "jogo_1"
+let corpo1 = document.createElement("div");
+corpo1.className = "jogo_1"
+let corpo2 = document.createElement("div");
+corpo2.className = "jogo_1"
 
 const options = {
   method: 'GET',
@@ -24,11 +32,32 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
 
   .then(response => response.json())
   .then(function (response){
-    // document.getElementById("thumbnail").src = response[0].thumbnail;
+    let nome_jogos = response[0].title;
+    let img_jogos =  response[0].thumbnail;
+    let descricao_jogos =  response[0].short_description;
+    // console.log(response);
+
+    let conteudo = `<img src="${img_jogos}" alt="" id="thumbnail">
+                    <p id="title">${nome_jogos}</p>
+                    <p id="short_description">${descricao_jogos}</p>`
+
+    corpo.innerHTML = conteudo;
+    pai_de_todos.appendChild(corpo);
+
+    corpo1.innerHTML = conteudo;
+    pai_de_todos.appendChild(corpo1);
+
+    corpo2.innerHTML = conteudo;
+    pai_de_todos.appendChild(corpo2);
   })
 
   .catch(err => console.error(err))
 
+  
 
+
+
+
+  
 
 
