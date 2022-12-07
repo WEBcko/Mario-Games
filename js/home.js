9efa5a430e7a256a50ae6a480d689d134c5a000e
TEXTBr <FireWitch2003@outlook.com>
2022-12-06 22:29:43 -0300

    isso

--- js/home.js
+++ js/home.js
@@ -8,14 +8,13 @@
 // })
 
 // .catch(() => window.alert(`error`));
+let agora = 3;
 
 const short = document.getElementById("botao_pvp");
 
 let pai_de_todos = document.getElementById("home_jogos");
 let corpo1 = document.createElement("div");
 corpo1.className = "jogo_1"
-
-
 
 const options = {
   method: 'GET',
@@ -28,27 +27,25 @@ const options = {
 
 const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games'
 
-let teste = 3;
 
 function consultar_API() {
   fetch(url, options)
     .then(response => response.json())
     .then(function (response) {
-      console.log(response);
-      MostrarJogos(response); 
+      // console.log(response);
+      MostrarJogos(response);
     })
-
     .catch(err => console.error(err))
     
 }
 
-consultar_API();
-
 function MostrarJogos(response) {
-  for(i=0; i < 300;i++){
+  
+  for(i=0; i < agora;i++){
      let corpo = document.createElement("div");
      corpo.id = `jogo_${i}`
      corpo.className = "jogo_1"
+
      let conteudo = `<img src="${response[i].thumbnail}" alt="" id="thumbnail">
                     <p id="title" class="title">${response[i].title}</p>
                     <p id="short_description" class="short_description">${response[i].short_description}</p>`
@@ -57,26 +54,19 @@ function MostrarJogos(response) {
      pai_de_todos.appendChild(corpo);
    }
 
-
-  // let conteudo = `<img src="${response[0].thumbnail}" alt="" id="thumbnail">
-  //                   <p id="title">${response[0].title}</p>
-  //                   <p id="short_description">${response[0].short_description}</p>`
-
-  // corpo.innerHTML += conteudo;
-  // pai_de_todos.appendChild(corpo);
-
-  // corpo1.innerHTML = conteudo;
-  // pai_de_todos.appendChild(corpo1);
-
-  // corpo2.innerHTML = conteudo;
-  // pai_de_todos.appendChild(corpo2);
-
+   agora +=3;
 }
 
 
 
+// function adicionar(){
+//   agora = teste;
+//   teste = teste + 3;
+//   console.log(teste)
+// }
 
-// short.addEventListener("click", );
+
+short.addEventListener("click", consultar_API);
 
 
 
