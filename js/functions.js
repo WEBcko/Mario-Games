b50c4fe39892433dfb48a2683701d75e0c24b3cb
emilio Anastácio de Paula Correa <emilioanastacio@gmail.com>
2022-12-08 13:35:54 -0300

    e meu porra

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,31 @@
 
 // Url da API
-const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games'
+const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=popularity'
 
-const video_banner = document.getElementsByName("teste_imagem")
+
 const load_more = document.getElementById("botao_carregar_mais");
+
+
+
+let contador = 0;
+
+
+//funcao de favoritos (salvo em LOCAL STORAGE)
+function favoritos(a) {
+    //pega o item "favoritos" no LocalStorage
+    let favoritos = localStorage.getItem("favoritos");
+    //transforma o JSON em Array para ser utilizado 
+    let dale = JSON.parse(favoritos);
+    console.log(dale);
+    //procura o valor o ID do jogo
+    let index = dale.indexOf(a.value);
+    console.log(index);
+    //se o ID do jogo for encontrado entao deleta ele do array, se nao existir o ID do jogo adiciona ele ao arry
+    index != -1 ? dale.splice(index, 1) : dale.push(a.value);
+    //Salva ele no LocalStorage novamente 
+    localStorage.setItem('favoritos', JSON.stringify(dale))
+
+}
 
 // Parametros para consulta na API, metodo e header
 const options = {
@@ -29,7 +51,7 @@ async function consultar_API() {
 }
 
 
-let agora = 0;
+let agora = 1;
 
 const botoes_categoria = document.querySelectorAll(".games");
 botoes_categoria.forEach(el => el.addEventListener('click', MostrarJogos(this)));
@@ -39,28 +61,53 @@ async function MostrarJogos(el) {
     let data = await consultar_API();
     console.log(el);
 
+    // aqui sera mostrado o abnner de jogo em destaque//
+    const jogo_banner = document.getElementById("home_jogo_destaque");
+
+    let conteudo_jogo_destaque = `<img id="teste_imagem" src="${data[0].thumbnail}" alt="">
+                                      <video autoplay="true" loop="true" id="video_destaque">
+                                        <source src="https://www.freetogame.com/g/${data[0].id}/videoplayback.webm" type="video/webm">
+                                      </video>
+                                      <div class="div_jogo_destaque_descricao">
+                                          <div class="alinha_botao">
+                                            <p class="jogo_destaque_conteudo">${data[0].title}</p>
+                                            <div class="div_jogos_destaque_botao_favoritos jogo_destaque_conteudo">
+                                               <button type="button"  value="${data[0].id}" onclick="favoritos(this)" >FAVORITAR</button>
+                                            </div>
+                                          </div>
+                                          <p class="jogo_destaque_conteudo">${data[0].short_description}</p>
+                                      </div>`
+
+
+    jogo_banner.innerHTML = conteudo_jogo_destaque;
+    //fim do banner de destaque//
+
     let pai_de_todos = document.getElementById("home_jogos");
 
-    for (i = agora; i < agora + 3; i++) {
+    for (i = agora; i < agora + 10; i++) {
         let corpo = document.createElement("div");
         corpo.id = `jogo_${i}`
         corpo.className = "jogo"
 
         let conteudo = `<a href="#user" class="jogo_conteudo">
-                        <img src="${response[i].thumbnail}" alt="" id="thumbnail">
-                        <p id="title" class="title">${response[i].title}</p>
-                        <p id="short_description" class="short_description">${response[i].short_description}</p>
-                        </a> `
+                        <img src="${data[i].thumbnail}" alt="" id="thumbnail">
+                        <p id="title" class="title">${data[i].title}</p>
+                        <p id="short_description" class="short_description">${data[i].short_description}</p>
+                        </a> 
+                        <button type="button"  value="${data[i].id}" onclick="favoritos(this)" >FAVORITAR</button>`
 
         corpo.innerHTML += conteudo;
         pai_de_todos.appendChild(corpo);
-    }
 
+
+    }
+    
     agora += 10;
+    
 }
 
 MostrarJogos();
 
+load_more.addEventListener("click", MostrarJogos);
 
-load_more.addEventListener("click", consultar_API);
 
