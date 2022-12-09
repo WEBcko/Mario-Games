0617fa5162d641c41c3683ae917d9edd58b03115
TEXTBr <FireWitch2003@outlook.com>
2022-12-08 21:20:55 -0300

    atualizacao do codigo de favoritos

--- js/functions.js
+++ js/functions.js
@@ -10,22 +10,7 @@ const load_more = document.getElementById("botao_carregar_mais");
 let contador = 0;
 
 
-//funcao de favoritos (salvo em LOCAL STORAGE)
-function favoritos(a) {
-    //pega o item "favoritos" no LocalStorage
-    let favoritos = localStorage.getItem("favoritos");
-    //transforma o JSON em Array para ser utilizado 
-    let dale = JSON.parse(favoritos);
-    console.log(dale);
-    //procura o valor o ID do jogo
-    let index = dale.indexOf(a.value);
-    console.log(index);
-    //se o ID do jogo for encontrado entao deleta ele do array, se nao existir o ID do jogo adiciona ele ao arry
-    index != -1 ? dale.splice(index, 1) : dale.push(a.value);
-    //Salva ele no LocalStorage novamente 
-    localStorage.setItem('favoritos', JSON.stringify(dale))
 
-}
 
 // Parametros para consulta na API, metodo e header
 const options = {
@@ -38,7 +23,6 @@ const options = {
 
 async function consultar_API() {
     try {
-
         const response = await fetch(url, options);
         let data = await response.json();
 
@@ -54,14 +38,14 @@ async function consultar_API() {
 let agora = 1;
 
 const botoes_categoria = document.querySelectorAll(".games");
-botoes_categoria.forEach(el => el.addEventListener('click', MostrarJogos(this)));
+botoes_categoria.forEach(el => el.addEventListener('click', MostrarJogos));
 
 async function MostrarJogos(el) {
 
     let data = await consultar_API();
     console.log(el);
 
-    // aqui sera mostrado o abnner de jogo em destaque//
+    // aqui sera mostrado o banner de jogo em destaque//
     const jogo_banner = document.getElementById("home_jogo_destaque");
 
     let conteudo_jogo_destaque = `<img id="teste_imagem" src="${data[0].thumbnail}" alt="">
@@ -72,7 +56,7 @@ async function MostrarJogos(el) {
                                           <div class="alinha_botao">
                                             <p class="jogo_destaque_conteudo">${data[0].title}</p>
                                             <div class="div_jogos_destaque_botao_favoritos jogo_destaque_conteudo">
-                                               <button type="button"  value="${data[0].id}" onclick="favoritos(this)" >FAVORITAR</button>
+                                               <button type="button" value="${data[0].id}" onclick="favoritos(this)" >FAVORITAR</button>
                                             </div>
                                           </div>
                                           <p class="jogo_destaque_conteudo">${data[0].short_description}</p>
@@ -87,24 +71,34 @@ async function MostrarJogos(el) {
     for (i = agora; i < agora + 10; i++) {
         let corpo = document.createElement("div");
         corpo.id = `jogo_${i}`
-        corpo.className = "jogo"
+        corpo.className = `jogo`
 
-        let conteudo = `<a href="#user" class="jogo_conteudo">
+        let conteudo = `<div>
+                        <a href="#user" class="jogo_conteudo">
                         <img src="${data[i].thumbnail}" alt="" id="thumbnail">
-                        <p id="title" class="title">${data[i].title}</p>
-                        <p id="short_description" class="short_description">${data[i].short_description}</p>
                         </a> 
-                        <button type="button"  value="${data[i].id}" onclick="favoritos(this)" >FAVORITAR</button>`
+                        <div class="div_jogo_conteudo_footer"><p id="title" class="title">${data[i].title}</p>
+                        
+                        <button type="button"  value="${data[i].id}" onclick="favoritos(this)" >FAVORITAR</button></div>
+                        </div>
+                        `
 
         corpo.innerHTML += conteudo;
         pai_de_todos.appendChild(corpo);
 
+        // <p id="short_description" class="short_description">${data[i].short_description}</p>
 
     }
-    
+
     agora += 10;
-    
+
 }
+
+
+
+
+
+
 
 MostrarJogos();
 
